d22e2f231259d5b69755927e370b75ca4156892f
stecape <ste.cape@gmail.com>
2016-12-06 09:49:19 +0000

    From Work

---
+++ client/imports/app/pages/controller/controller.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+ 
+@Component({
+  selector: 'controller',
+  templateUrl: './controller.component.html'
+})
+export class ControllerComponent implements OnInit {
+ 
+  constructor() { }
+
+  ngOnInit() { }
+}
---
+++ client/imports/app/pages/water-plant/waterplant.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+ 
+import template from './waterplant.component.html';
+ 
+@Component({
+  selector: 'water-plant',
+  template
+})
+export class WaterPlantComponent implements OnInit {
+ 
+  constructor() { }
+
+  ngOnInit() { }
+}
